3039b9b0d6b946ab0dc40698e43e526eb23f5aa0
rohith-rdx <rohithdrizex123@gmail.com>
2022-03-26 21:21:32 +0530

    error corrections

--- dimenzions/app/urls.py
+++ dimenzions/app/urls.py
@@ -6,7 +6,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns=[
-    re_path(r'^$', views.category, name="category"),
+    re_path(r'^category$', views.category, name="category"),
     re_path(r'^add_category$', views.add_category, name="add_category"),
     re_path(r'^cat_delete/(?P<cat_id>\d+)$', views.cat_delete, name="cat_delete"),
 
